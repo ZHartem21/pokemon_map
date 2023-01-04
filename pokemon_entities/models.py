666d84fbfbfea43cf495330a0c8b43ba6480d593
ZHartem21 <zhartem21@gmail.com>
2023-01-04 03:05:30 +0300

    Add previous evolution

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -7,6 +7,12 @@ class Pokemon(models.Model):
     title_en = models.CharField(max_length=200, blank=True)
     title_jp = models.CharField(max_length=200, blank=True)
     description = models.TextField(blank=True)
+    previous_evolution = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
         return self.title
